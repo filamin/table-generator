506393ee684b4d63203d0733b51ee1764df3a8c5
Филипп Минаев <f.minaev@b1advisor.com>
2021-10-21 20:44:24 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'new deploy'
+git push -f git@github.com:filamin/table-generator.git master:gh-pages
+
+cd -
